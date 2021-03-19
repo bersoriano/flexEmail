cb6120f4f9133f68d035c2acbff52ca7a84b48eb
Bernardo Soriano <bersoriano@me.com>
2021-03-19 14:00:34 -0600

    fixing basic2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "617bf7c9d55bd7a14c59";
+/******/ 	var hotCurrentHash = "f0c648a1c646b8eaf4b0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
