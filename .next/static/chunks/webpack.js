a0c2f9d074daceb3ba9d21d0ee80e6252f382fab
Bernardo Soriano <bersoriano@me.com>
2021-03-22 09:44:00 -0600

    Updated testing url

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1bf6f37dc656aab171f1";
+/******/ 	var hotCurrentHash = "aa684881989f49e9376a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
