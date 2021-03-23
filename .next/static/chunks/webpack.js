acbd5d4125290b6d62d8fe956a65e7284a2c9137
Bernardo Soriano <bersoriano@me.com>
2021-03-23 17:03:42 -0600

    updated templates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bbcfea92074ca477ef8d";
+/******/ 	var hotCurrentHash = "6398a48f1b9f73e7ceb6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
