dc63a5359e514597d58a2e7c78c0494186c1f53f
Bernardo Soriano <bersoriano@me.com>
2021-04-08 12:05:47 -0500

    new april templates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "36cadc82760646909bbb";
+/******/ 	var hotCurrentHash = "8a0d0a3eb8756dafed18";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -5,180 +5,152 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>March Templates</title>
+        <title>Flex Templates</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header></Header>
       <MainContainer>
-      <h2>Non Premium templates</h2>
+        <h2>April Email</h2>
         <ul className="template-container">
           <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template9/template9-austin.html">
-            </iframe>                              
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/april/template1/template1.html"
+            ></iframe>
           </li>
           <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template9/template9-charlotte.html">
-            </iframe>                              
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/april/template2/template2.html"
+            ></iframe>
           </li>
           <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template9/template9-santonio.html">
-            </iframe>                              
-          </li>          
-
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template1/Do%20More%20Email%201.html">
-            </iframe>                              
-          </li>
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template2/Do%20More%20Email%202.html">
-            </iframe>                              
-          </li>
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template7/template7.html">
-            </iframe>                              
-          </li> 
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template8/all-set-confirmation.html">
-            </iframe>                              
-          </li>                                  
-        </ul>        
-
-        <h2>Premium Templates</h2>
-
-        <ul className="template-container">
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template3/flexPremium.html">
-            </iframe>                              
-          </li>
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template4/flexPremium2.html">
-            </iframe>                              
-          </li>
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template5/flexPremium3.html">
-            </iframe>                              
-          </li> 
-          <li>
-            <iframe id="inlineFrameExample"
-                title="Inline Frame Example"
-                width="600"
-                height="700"
-                src="https://flex-email.vercel.app/templates/march/template6/flexpremium4.html">
-            </iframe>                              
-          </li>                                  
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/april/template3/template3.html"
+            ></iframe>
+          </li>                    
         </ul>
-        {/* <ul>
-          <li>
-            <a href="/templates/march/template1/Do More Email 1.html">
-              Template 1
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/TNFN2RNk6O">
-              E-mail previews
-            </a>
-          </li>     
-          <li>
-            <a href="/templates/march/template2/Do More Email 2.html">
-              Template 2
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/85NNSLgz4s">
-              E-mail previews
-            </a>
-          </li>  
-          <li>
-            <a href="/templates/march/template7/template7.html">
-              Survey Template
-            </a>
 
-          </li> 
+        <h2>March - Non Premium Email</h2>
+        <ul className="template-container">
           <li>
-            <a href="/templates/march/template8/all-set-confirmation.html">
-              Asset Email Confirmation
-            </a>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template9/template9-austin.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template9/template9-charlotte.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template9/template9-santonio.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template1/Do%20More%20Email%201.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template2/Do%20More%20Email%202.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template7/template7.html"
+            ></iframe>
+          </li>
+          <li>
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template8/all-set-confirmation.html"
+            ></iframe>
+          </li>
+        </ul>
 
-          </li>                                                       
-        </ul> */}
-        {/* <h3>Premium Templates</h3> 
-        <ul>
+        <h2>March - Premium Email</h2>
+        <ul className="template-container">
           <li>
-            <a href="/templates/march/template3/flexPremium.html">
-              Template 1 - Premium
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/vT12OvaGRw">
-              E-mail previews
-            </a>            
-          </li>   
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template3/flexPremium.html"
+            ></iframe>
+          </li>
           <li>
-            <a href="/templates/march/template4/flexPremium2.html">
-              Template 2 - Premium
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/UjGDAS4lyK">
-              E-mail previews
-            </a>             
-          </li>  
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template4/flexPremium2.html"
+            ></iframe>
+          </li>
           <li>
-            <a href="/templates/march/template5/flexPremium3.html">
-              Template 3 - Premium
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/Jj7vjT8q08">
-              E-mail previews
-            </a>                         
-          </li>  
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template5/flexPremium3.html"
+            ></iframe>
+          </li>
           <li>
-            <a href="/templates/march/template6/flexpremium4.html">
-              Template 4 - Premium.
-            </a>
-            <a href="https://app.emailonacid.com/shared-preview/x6QY2XtBe0">
-              E-mail previews
-            </a>            
-          </li>                                                  
-        </ul> */}
-
+            <iframe
+              id="inlineFrameExample"
+              title="Inline Frame Example"
+              width="600"
+              height="700"
+              src="https://flex-email.vercel.app/templates/march/template6/flexpremium4.html"
+            ></iframe>
+          </li>
+        </ul>
       </MainContainer>
-      <footer>
-      </footer>
+      <footer></footer>
 
       <style jsx>{`
-
         h2 {
           text-align: center;
           margin: 1em 0;
@@ -195,19 +167,19 @@ export default function Home() {
           width: 80vw;
           max-width: 80vw;
           overflow-x: scroll;
-          margin: 0 auto;          
+          margin: 0 auto;
         }
         ul li.template-container {
           display: flex;
           width: 80vw;
           max-width: 80vw;
           overflow-x: scroll;
-          margin: 0 auto;      
+          margin: 0 auto;
         }
         ul li {
           display: flex;
           flex-direction: row;
-          padding: .5em 0;
+          padding: 0.5em 0;
           max-width: 600px;
           margin: 0 1em;
           border-bottom: 1px solid #e9e9e9;
