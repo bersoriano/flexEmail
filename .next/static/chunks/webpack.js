863a9c1e6214142501905e749564747d092a3960
Bernardo Soriano <bersoriano@me.com>
2021-04-28 13:56:10 -0500

    fixing mobile bug

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "91138d9126661d752d4a";
+/******/ 	var hotCurrentHash = "047868210244c8e8809c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
