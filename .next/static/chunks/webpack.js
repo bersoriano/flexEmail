295591c09a6f6e7926be5d9e3d1faae8a6a4d890
Bernardo Soriano <bersoriano@me.com>
2021-06-19 16:17:19 -0500

    minor fixes for templates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "2012ba95bf9a3241491e";
+/******/ 	var hotCurrentHash = "05beeb5376c2f356d3b1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
